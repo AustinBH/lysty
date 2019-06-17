34280fbd3dcdd1b183573a3a5eb6ca06577114d7
AidiRi <rivera.aidi@gmail.com>
2019-06-17 16:43:22 -0700

    Seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+5.times do
+User.create(
+  name: Faker::Games::SuperSmashBros.fighter,
+  email: Faker::Internet.email
+)
+end
+
+5.times do
+List.create(
+  title: Faker::Space.star,
+  user: User.all.sample
+)
+end
+
+10.times do
+Product.create(
+  name: Faker::Commerce.product_name,
+  price: Faker::Commerce.price,
+  origin: Faker::Address.country
+)
+end
+
+10.times do
+Store.create(
+  name: "#{Faker::Color.color_name.capitalize} #{Faker::Superhero.power}",
+  location: Faker::Address.full_address
+)
+end
