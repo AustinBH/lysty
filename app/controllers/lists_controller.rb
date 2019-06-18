9d1acfdf4723dc2a5622ba1974b507b3e5808221
AidiRi <rivera.aidi@gmail.com>
2019-06-18 08:49:35 -0700

    list controller updated

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -11,6 +11,16 @@ class ListsController < ApplicationController
     render :show
   end
 
+  def edit
+    @list = List.find(params[:id])
+    @products = @list.products
+  end
 
+  def update
+  end
 
+  def destroy
+    @list.delete
+    redirect_to '/' #Not redirecting to root, redirects to @list ??????
+  end
 end
