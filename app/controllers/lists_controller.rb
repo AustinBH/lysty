45395efb475faf006c5084b80620e74b6c14ada3
AidiRi <rivera.aidi@gmail.com>
2019-06-17 17:57:16 -0700

    list controller's index and show actions

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,16 @@
 class ListsController < ApplicationController
 
+  def index
+    @lists = List.all
+    render :index
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @products = @list.products
+    render :show
+  end
+
 
 
 end
